26e7760c1949fa423b0aa707bd15ca5070fff490
Grace Lee <igracelee1@gmail.com>
2019-08-31 00:49:56 -0400

    insert method

--- Hash Tables/hashTable.js
+++ Hash Tables/hashTable.js
@@ -10,13 +10,13 @@ function HashTable(size) {
     this.buckets = Array(size);
     // keep track of how many buckets we have in our hash table
     this.numBuckets = this.buckets.length;
-}
+};
 
 function HashNode(key, value, next) {
     this.key = key;
     this.value = value;
     this.next = next || null; // if no next parameter (next node) is passed in, set to null
-}
+};
 
 HashTable.prototype.hash = function(key) {
     let total = 0; // initiate total variable
@@ -27,7 +27,20 @@ HashTable.prototype.hash = function(key) {
     // total is a large # now, but we need to make it a # corresponding to a bucket #
     let bucket = total % this.numBuckets; // divide any # by 30, and remainder will be a # between 0 and 29
     return bucket;
-}
+};
+
+HashTable.prototype.insert = function(key, value) {
+    let index = this.hash(key); // we know what bucket our hash node will go in
+    if (!this.buckets[index]) this.buckets[index] = new HashNode(key, value);
+    // if there is a node or multiple nodes chained to the bucket...
+    else {
+        let currNode = this.buckets[index];
+        while (currNode.next) { // run while there's a node in the chain
+            currNode = currNode.next;
+        }
+        currNode.next = new HashNode(key, value);
+    }
+};
 
 const myHT = new HashTable(30);
 
